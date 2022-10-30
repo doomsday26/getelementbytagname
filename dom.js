e7fed56e07d6092a4a42769e0b3675c605afcdbb
doomsday26 <harshnamdeo261999@gmail.com>
2022-10-30 16:43:12 +0530

    queryselector and queryselectorAll

--- dom.js
+++ dom.js
@@ -1,10 +1,42 @@
-var li= document.getElementById('items')
-const append =document.createElement("li")
-append.innerText="item 5"
-append.className="list-group-item";
-li.appendChild(append);
+//query selector and query selector all
 
-// let items= document.getElementsByClassName("list-group-item");
-// items[4].style.backgroundColor="aqua"
-let tag= document.getElementsByTagName('li');
-tag[4].style.backgroundColor="aqua";
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom='2px solid black'
+
+// var input= document.querySelector('input');
+// input.value='hello world';
+
+// var submit =document.querySelector('input[type="submit"]');
+// submit.value='SEND'
+
+// var item= document.querySelector(".list-group-item")
+// item.style.color='red';
+
+// var lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color='green';
+
+//second child bg color green
+// var lastItem = document.querySelector('.list-group-item:nth-child(2)');
+// lastItem.style.backgroundColor='green' ;
+// //third item display none
+// var lastItem = document.querySelector('.list-group-item:nth-child(3)');
+// lastItem.style.display='none' ;
+
+
+//queryselector all
+var titles = document.querySelectorAll('.title');
+console.log(titles);
+titles[0].textContent='hello'
+ 
+
+var items= document.querySelectorAll('li');
+console.log(items);
+items[1].style.color= 'green'
+
+var odd= document.querySelectorAll('li:nth-child(odd)');
+
+
+for (let i = 0; i < odd.length; i++) {
+   odd[i].style.backgroundColor='green';
+    
+}
